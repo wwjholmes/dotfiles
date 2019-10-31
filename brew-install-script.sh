b18c59e1e3cd3a04699ce62c88a133d130a086aa
wenjingw <wenjingw@whatsapp.com>
2019-10-31 10:43:39 -0700

    [laptop] Add zsh plugins, powerline, powerline fonts to install.sh

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -10,7 +10,7 @@ echo "Installing brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing cask"
-brew tap caskroom/cask
+brew tap homebrew/cask-cask
 
 # Install daily work softwares
 brew cask install 1password
@@ -27,3 +27,21 @@ brew cask install tweetbot
 brew cask install wechat
 brew cask install whatsapp
 brew cask install xscope
+
+# Install zsh plugins
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
+
+# Install powerlevel9k for Oh-My-ZSH
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install powerline fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
