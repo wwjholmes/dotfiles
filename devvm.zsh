36c26a72ff520dc6f2abca0325e9e135306d44e2
wenjingw <wenjingw@whatsapp.com>
2020-02-01 16:35:47 -0800

    [devserver] Add git alias & comment out tmux auto

--- devvm.zsh
+++ devvm.zsh
@@ -1,9 +1,17 @@
 . /home/wenjingw/local/whatsapp/server/erl/tools/waserver_bashrc
 
-if [[ ! $TMUX && -t 0 && $TERM_PROGRAM != nuclide ]]; then
-    tmux $TMUX_OPTIONS new-session -As auto
-fi
+#if [[ ! $TMUX && -t 0 && $TERM_PROGRAM != nuclide ]]; then
+#    tmux $TMUX_OPTIONS new-session -As auto
+#fi
 
 if [ -d "$HOME/.local/vim/bin/" ] ; then
     PATH="$HOME/.local/vim/bin/:$PATH"
 fi
+
+alias gn='git number --column'
+alias ga='git number add'
+
+alias er='erlcluster'
+alias pinfo='erlcanary info pmtdev'
+alias cinfo='erlcanary info cdev'
+
