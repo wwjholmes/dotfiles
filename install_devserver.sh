9484ade185a0c3da7b2f0916209cabbe826ef5dd
wenjingw <wenjingw@whatsapp.com>
2020-05-03 23:23:21 -0700

    [devserver] add oh-my-zsh to install script

--- install_devserver.sh
+++ install_devserver.sh
@@ -1,3 +1,6 @@
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Install zsh plugins
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
